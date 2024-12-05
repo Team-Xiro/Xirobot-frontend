7ea1fb2a49defd5c25100a78c9ab93b352242ad0
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-12-05 13:06:58 +0530

    ChatInputBox created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ChatInputBox from './components/ChatInputBox';
 
 function App() {
   return (
     <div className="App">
-      
+      <ChatInputBox/>
     </div>
   );
 }
