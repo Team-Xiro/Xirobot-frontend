c49b9aef7b6839da56fa1c4e0cdc083466137dfc
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-12-06 23:15:18 +0530

    voicechat mic icon removed

--- src/components/VoiceChat.jsx
+++ src/components/VoiceChat.jsx
@@ -27,9 +27,6 @@ const VoiceChat = () => {
       </div>
       <div className="flex items-center justify-center -mt-36">
         <button className="relative w-12 h-12 mx-2 bg-white rounded-md">
-          <MicrophoneIcon className="absolute w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer top-1/2 left-1/2" />
-        </button>
-        <button className="relative w-12 h-12 mx-2 bg-white rounded-md">
           <XMarkIcon className="absolute w-8 h-8 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer top-1/2 left-1/2" />
         </button>
       </div>
